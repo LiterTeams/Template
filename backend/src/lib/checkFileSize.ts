083d4914253bc8dc14d4fc148b2aa57603aa410b
LiterTeams <LiterTeams@gmail.com>
2025-01-22 21:15:53 +0300

    Update checkFileSize.ts

--- backend/src/lib/checkFileSize.ts
+++ backend/src/lib/checkFileSize.ts
@@ -1,13 +1,11 @@
 type UnitEnumT = "B" | "BT" | "KB" | "MB" | "GB" | "TB";
-import fileConfig from "src/const/fileConfig";
+import config from "src/const/files";
 
 const checkFileSize = (fileSize: number ,min:string = "0 MB", max:string = "0 MB") => {
-    const units = {min:fileConfig.sizes[min.split(" ")[1] as UnitEnumT], max:fileConfig.sizes[max.split(" ")[1] as UnitEnumT]}
+    const units = {min:config.units[min.split(" ")[1] as UnitEnumT], max:config.units[max.split(" ")[1] as UnitEnumT]}
     
     const minSize = units.min * Number(min.split(" ")[0]);
     const maxSize = units.max * Number(max.split(" ")[0]);
-
-    console.log(minSize, maxSize, fileSize);
     
     if (minSize + maxSize === 0) return true;
     if(minSize >= 0 && maxSize > 0) return fileSize >= minSize && fileSize <= maxSize;
