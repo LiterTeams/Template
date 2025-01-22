193b9fec2bc2e296b902a8518fa7d8319127b1f5
LiterTeams <LiterTeams@gmail.com>
2025-01-22 21:16:20 +0300

    Update useVideoPlayer.ts

--- client/src/features/hooks/system/useVideoPlayer.ts
+++ client/src/features/hooks/system/useVideoPlayer.ts
@@ -1,26 +1,28 @@
 "use client";
-import { useState, useEffect, useCallback, useRef } from "react";
+import { useState, useEffect, useCallback, useRef, ChangeEvent } from "react";
 
 import durationConver from "@lib/durationConver";
 
-export default function useVideoPlayer(autoPlay: boolean = false, mute: boolean = false, volume: number = 0.1){
+export default function useVideoPlayer(autoPlay: boolean = false, mute: boolean = true, volume: number = .5){
     const videoRef = useRef<HTMLVideoElement | null>(null);
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [isEnded, setIsEnded] = useState(false);
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+    const [isFullscreen, setIsFullscreen] = useState(false);
+    const [isEnded, setIsEnded] = useState<boolean>(false);
+    const [progress, setProgress] = useState<number>(0);
+    const [videoVolume, setVideoVolume] = useState<number>(volume);
+    const [isMuted, setIsMuted] = useState<boolean>(mute);
 
-    const [time, setTime] = useState(0);
-    const [duration, setDuration] = useState(0);
+    const [currentVideoTime, setCurrentVideoTime] = useState<number>(0);
+    const [totalVideoTime, setTotalVideoTime] = useState<number>(0);
 
-    const [currentDurationl, setCurrentDuration] = useState("00:00:00");
-    const [totalDuration, setTotalDuration] = useState("00:00:00");
-    const [frames, setFrames] = useState(0);
+    const [currentVideoDurationl, setCurrentVideoDuration] = useState<string>("00:00:00");
+    const [totalVideoDuration, setTotalVideoDuration] = useState<string>("00:00:00");
 
     const play = useCallback(() => {
         if (!videoRef.current) return;
-        videoRef.current.volume = mute ? 0 : volume;
         videoRef.current.play();
         setIsPlaying(true);
-    }, [mute, volume]);
+    }, []);
     
     const pause = useCallback(() => {
         if (!videoRef.current) return;
@@ -28,39 +30,49 @@ export default function useVideoPlayer(autoPlay: boolean = false, mute: boolean 
         setIsPlaying(false);
     }, []);
 
-    const toggle = useCallback(() => {
+    const togglePlay = useCallback(() => {
         if (!videoRef.current) return;
         return !isPlaying ? play() : pause();
     }, [isPlaying, play, pause]);
 
-    const globalUpdate = () => {
+    const toggleFullScreen = () => setIsFullscreen(!isFullscreen);
+
+    const handleVolumeChange = (event:ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        if (!videoRef.current) return;
+        setVideoVolume(parseFloat(event.target.value));
+        videoRef.current.volume = videoVolume;
+    }
+
+    const handelMuteChange = () => setIsMuted(!isMuted);
+
+    const onLoadedMetadata = () => {
         if (!videoRef.current) return;
         updateTotalDuration();
-        updateCurrentDuration();
-        updateVideoFrames();
+        onTimeUpdate();
     }
 
     const updateTotalDuration = () => {
         if (!videoRef.current) return;
-        setDuration(videoRef.current.duration);
-        setTotalDuration(durationConver(videoRef.current.duration));
+        // if (isNaN(videoRef.current.duration)) return;
+        const duration = videoRef.current.duration;
+        setTotalVideoTime(duration);
+        setTotalVideoDuration(durationConver(duration));
     };
 
-    const updateCurrentDuration = () => {
+    const onTimeUpdate = () => {
         if (!videoRef.current) return;
-        setTime(videoRef.current.currentTime);
-        setCurrentDuration(durationConver(videoRef.current.currentTime));
-        if (time == duration) setIsEnded(true);
-    }
-
-    const updateVideoFrames = () => {
-        if (!videoRef.current) return;
-        setFrames(videoRef.current.currentTime * 24);
+        if (totalVideoTime == 0) updateTotalDuration();
+        const currentTime = videoRef.current.currentTime;
+        setCurrentVideoTime(currentTime);
+        setCurrentVideoDuration(durationConver(currentTime));
+        setProgress((currentTime / totalVideoTime) * 100);
+        if (currentVideoTime == totalVideoTime || progress >= 100) setIsEnded(true);
     }
 
     useEffect(() => {
-        if (autoPlay) toggle();
-    }, [isPlaying, totalDuration, frames, autoPlay, toggle]);
+        if (autoPlay && !isPlaying) togglePlay();
+    }, [videoRef, isPlaying, autoPlay, togglePlay]);
 
-    return { isPlaying, isEnded, videoRef, frames, time, duration, currentDurationl, totalDuration, toggle, updateCurrentDuration, globalUpdate }
+    return { isPlaying, isFullscreen, videoVolume, isMuted, isEnded, progress, videoRef, currentVideoTime, totalVideoTime, currentVideoDurationl, totalVideoDuration, togglePlay, toggleFullScreen, handleVolumeChange, handelMuteChange, onTimeUpdate, onLoadedMetadata }
 }
