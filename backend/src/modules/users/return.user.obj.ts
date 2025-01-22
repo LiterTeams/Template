46b24a30941dbb8a2373a987d454d7d6a4f2b047
LiterTeams <LiterTeams@gmail.com>
2025-01-22 21:15:33 +0300

    Update return.user.obj.ts

--- backend/src/modules/users/return.user.obj.ts
+++ backend/src/modules/users/return.user.obj.ts
@@ -4,7 +4,6 @@ export const returnUserObj:Prisma.UserSelect = {
     id: true,
     nickname: true,
     email: true,
-    privilege: { select: { project: true, role: true } },
     blocking: true,
     created_at: true,
 }
