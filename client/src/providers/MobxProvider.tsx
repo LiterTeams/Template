0ca1876423a5f1fbee1e674856eae7f6904f9e11
LiterTeams <LiterTeams@gmail.com>
2025-01-22 21:16:24 +0300

    Update MobxProvider.tsx

--- client/src/providers/MobxProvider.tsx
+++ client/src/providers/MobxProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { MobxStoreContext  } from "@app/features/hooks/system/useMobxStore";
 import Store from "@features/store/store";
-export default function RootStoreProvider({children}:{children:React.ReactNode}){
+export default function MobxProvider({children}:{children:React.ReactNode}){
     return <MobxStoreContext.Provider value={new Store()}>{children}</MobxStoreContext.Provider>
 }
