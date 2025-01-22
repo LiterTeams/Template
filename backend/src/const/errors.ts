9134b9ce8e9bdc1f347fdcf3243e920b0b580ef4
LiterTeams <LiterTeams@gmail.com>
2025-01-22 21:14:38 +0300

    Create errors.ts

---
+++ backend/src/const/errors.ts
@@ -0,0 +1,5 @@
+const errors = {
+    extension: {message: "Неподдерживаемое расширение файла", error: "extension", status: 400},
+    size: {message: "Файл слишком большой или маленький", error: "size", status: 400};
+}
+export default errors;
