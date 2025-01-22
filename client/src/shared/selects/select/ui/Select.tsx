8907d8150a1e9a2c8a8c248fd7dcde195ad04c6d
LiterTeams <LiterTeams@gmail.com>
2025-01-22 21:16:34 +0300

    Update Select.tsx

--- client/src/shared/selects/select/ui/Select.tsx
+++ client/src/shared/selects/select/ui/Select.tsx
@@ -22,22 +22,24 @@ export default function Select({queryKey, initial = null, prefix = undefined, op
     return(
         <div className="relative text-sm">
             <button onClick={toggleShowOptions} className={`flex flex-center gap-1 px-3 py-2 rounded-xl min-w-28 duration-300 hover:bg-blue hover:drop-shadow-glow_blue ${showOptions ? "bg-blue drop-shadow-glow_blue" : "bg-dark-secondaty"}`}>
-                {initialOption ? `${prefix ? `${prefix} : ${initialOption.label}` : initialOption.label}` : "Select..."}
+                {prefix} {isLoading ? "Loading..." : (isError ? "Error" : (initialOption ? initialOption.label : "Select..."))}
             </button>
-            <div className={`absolute z-[2] left-0 top-12 w-full overflow-hidden bg-dark-secondaty ${showOptions ? "block" : "hidden"}`}>
-                <ul className="max-h-64 overflow-y-auto">
-                    {selectOptions.map((option, index) =>
-                        <li
-                            key={`option-${option.value}-${index}`}
-                            className={`flex gap-1 duration-300 hover:bg-blue px-3 py-2 cursor-pointer ${option.active && "bg-blue"}`}
-                            onClick={() => selectToggle(option.value)}
-                        >
-                            {option.icon}
-                            {option.label}
-                        </li>
-                    )}
-                </ul>
-            </div>
+            {!isLoading && !isError &&
+                <div className={`absolute z-[2] left-0 top-12 w-full overflow-hidden bg-dark-secondaty ${showOptions ? "block" : "hidden"}`}>
+                    <ul className="max-h-64 overflow-y-auto">
+                        {selectOptions.map((option, index) =>
+                            <li
+                                key={`option-${option.value}-${index}`}
+                                className={`flex gap-1 duration-300 hover:bg-blue px-3 py-2 cursor-pointer ${option.active && "bg-blue"}`}
+                                onClick={() => selectToggle(option.value)}
+                            >
+                                {option.icon}
+                                {option.label}
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            }
         </div>
     )
 }
