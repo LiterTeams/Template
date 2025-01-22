329b6bb957eda0e19a57ae8a35c68ac87597f4b4
LiterTeams <LiterTeams@gmail.com>
2025-01-22 21:13:21 +0300

    Update seeders.ts

--- backend/seeders/seeders.ts
+++ backend/seeders/seeders.ts
@@ -5,14 +5,6 @@ import * as dotenv from "dotenv";
 import privilegeSeeder from "./privilege.seeders";
 import userSeeder from "./user.seeders";
 
-import { tagSeeders, imageStorageTagsSeeders } from "./tag.seeders"
-
-import imageStorageSeeder from "./image.storage.seeders"; 
-import videoStorageSeeder from "./videos.storage.seeders";
-import noteStorageSeeder from "./note.storage.seeders";
-import audioStorageSeeder from "./audio.storage.seeders";
-import archiveStorageSeeder from "./archive.storage.seeders";
-
 // <------ Init ------> //
 
 const prisma = new PrismaClient();
@@ -25,30 +17,10 @@ const contentSeeder = async () => {
     await privilegeSeeder();
 }
 
-const storageSeeder = async () => {
-    await tagSeeders();
-    
-    await imageStorageSeeder();
-    // await imageStorageTagsSeeders();
-
-    await videoStorageSeeder();
-    // await videoStorageTagsSeeders();
-
-    await noteStorageSeeder();
-    // await noteStorageTagsSeeders();
-
-    await audioStorageSeeder();
-    // await audioStorageTagsSeeders();
-
-    await archiveStorageSeeder();
-    // await archiveStorageTagsSeeders();
-}
-
 const seeders = async () => {
     console.log("Start Seeding...");
 
     await contentSeeder();
-    await storageSeeder();
 
     console.log("End Seeding");
 }
