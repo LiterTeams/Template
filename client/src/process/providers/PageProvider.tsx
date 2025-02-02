32e0b88d33727e7f84a0f4d6f713a188f3a1f255
LiterTeams <LiterTeams@gmail.com>
2025-02-02 20:25:12 +0300

    Update PageProvider.tsx

--- client/src/process/providers/PageProvider.tsx
+++ client/src/process/providers/PageProvider.tsx
@@ -1,14 +1,5 @@
 "use client";
 
-import Header from "@widgets/header/Header";
-import Footer from "@widgets/footer/Footer";
-
 export default function PageProvider({children}:{children:React.ReactNode}){
-    return(
-        <div className="wrapper">
-            <Header />
-            <main>{children}</main>
-            <Footer />
-        </div>
-    )
+    return children;
 }
